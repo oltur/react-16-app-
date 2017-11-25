cf3c03993f50ebea2624df2c0dd8636dcfe32172
Alex Turevski <me@turevski.com>
2017-11-25 15:03:55 +0200

    Test fix

--- src/components/ui/Button/Button.test.jsx
+++ src/components/ui/Button/Button.test.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import test from 'tape';
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme';
+
+import Adapter from 'enzyme-adapter-react-16';
+
 import Button from './Button';
+
+configure({ adapter: new Adapter() });
 
 test('Button', (t) => {
   let clickTestValue = 0;
